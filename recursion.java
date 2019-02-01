76ca403d1ccd69e571a1f74f70a206927dcadc7d
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-01-31 19:04:28 -0500

    Trying to debug

--- recursion.java
+++ recursion.java
@@ -1,4 +1,10 @@
+import java.util.*;
 public class recursion{
+    public static void main(String[] args) {
+        System.out.println(sqrt(169, .0001));
+        System.out.println(fib(4));
+        System.out.println(makeAllSums(3));
+    }
     /*You may write additional private methods */
 
     /*Recursively find the sqrt using Newton's approximation
@@ -7,16 +13,20 @@ public class recursion{
 
     */
     public static double sqrt(double n, double tolerance){
-        double g = n/3;
-        if (n == 0){
-            return 0;
+            double g = n/3;
+            if (n == 0){
+                return 0;
+            }
+            return helper (n, g, tolerance);
         }
-        if (   ((Math.abs((g * g) - n)) / n) * 100 > .00001){
-            double help = ((n/g) + g)/2.0;
-          return sqrt (n, help);
+        public static double helper (double n, double g, double tolerance){
+            if (   ((Math.abs((g * g) - n)) / n) * 100 > tolerance){
+                double help = ((n/g) + g)/2.0;
+              return helper (n, help, tolerance);
+            }
+            return g;
         }
-        return g;
-    }
+
 
     /*Recursively find the n'th fibbonaci number in linear time
      *fib(0) = 1; fib(1) = 1; fib(5) = 5
@@ -24,7 +34,7 @@ public class recursion{
      */
     public static int fib(int n){
         if (n > 1){
-            return fibonacci(n - 2) + fibonacci (n - 1);
+            return fib(n - 2) + fib(n - 1);
         }
         if (n == 1){
             return 1;
@@ -56,14 +66,26 @@ public class recursion{
         // and then do the same for 2
         // 2 + 1
         // it is like the 3 is not included so this should work
-        if (n == 0){
-            return as;
-        }
+        if (n != 0){
+        // only works for half
+        // 1   2   3  tot
+        // T   T   T   6
+        // T   T   F   3
+        // T   F   F   1
+        // F   F   F   0
+        //If done other way
+        // F   T   T   5
+        // F   F   T   3
+
         as.add(rap (n));
+        System.out.println(as);
         makeAllSums(n - 1);
     }
+    return as;
+}
     public static int rap (int n){
         if (n != 0){
+            System.out.println(n);
         return n + rap (n - 1);
     }
     else return 0;
